31cd0ae09fb0e2e3bbdd6222d8ed12410d1ea3dc
Logan Zimmerman <lz128152@student.wpsok.org>
2024-08-22 13:42:30 -0500

    added hw2

---
+++ HW2.py
@@ -0,0 +1,17 @@
+#name: logan zimmerman
+#class: 6th hour
+#assignment: HW2
+
+
+print('hello world')
+
+
+n = (input("what is your name: "))
+if n == 'logan zimmerman' :print('ok')
+else :print("i know that's not your real name logan")
+
+
+z = float(input('enter first integer: '))
+b = float(input('enter second integer: '))
+g = z + b
+print(g)
