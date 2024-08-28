ac843ea60c2399d6de6192e4ccd8174daf8fe52a
Logan Zimmerman <lz128152@student.wpsok.org>
2024-08-28 13:57:14 -0500

    added HW3

---
+++ HW3.py
@@ -0,0 +1,12 @@
+print('hello world')
+list =  ['dom', 'rob', 'logan', 'nick', 'patt']
+list.append(input('enter your name: '))
+print(list[3])
+
+
+numList = [1, 3, 7, 4]
+numList.insert(1,int(input("Enter a number: ")))
+print(numList)
+numList.sort()
+sum = numList[0]+numList[1]+numList[2]
+print(sum)
