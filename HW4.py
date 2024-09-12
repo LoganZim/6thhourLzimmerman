1d0e7d7bc70550cb1b8888c1b2496f80435fca79
Logan Zimmerman <lz128152@student.wpsok.org>
2024-09-12 13:57:11 -0500

    added HW4

---
+++ HW4.py
@@ -0,0 +1,35 @@
+#logan zimmerman
+#6th hour
+#HW4
+
+print('hello world!')
+dict={
+    'logan' : "football player, " "male, " "14",
+    'jax' : "random kid, " "male, " "14 ",
+    'numbers' : [3, 4, 5]
+}
+print(dict['numbers'][2])
+dict.update({'jacob': 'new kid ' 'male ' '14' })
+print(dict)
+print(dict.clear())
+dict2={
+    'cj' : {
+        'sport ' : 'football player',
+        'title' : 'gonna be homecoming king',
+
+    },
+    'jax' : {
+        'sport' : 'good coder',
+        'place' : 'in 6th hour class',
+
+
+    },
+    'evan' : {
+        'place' : "in class",
+        'gender' : "male",
+
+
+    },
+
+}
+print(dict2)
