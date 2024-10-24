8fc4521c7fb1c0a86ec4553c002c9ff40aff6a4e
Logan Zimmerman <lz128152@student.wpsok.org>
2024-10-24 13:34:29 -0500

    added HW9

---
+++ HW9.py
@@ -0,0 +1,16 @@
+#Name:Logan
+#Class: 6th Hour
+#Assignment: HW9
+
+print('Hello World!')
+
+#2. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
+#https://adacomputerscience.org/images/content/computer_science/design_and_development/program_design/figures/ada_cs_design_flow_ifelseif.svg
+Temp = int(input('what is the temp: '))
+if Temp > 20:
+    print('its hot')
+elif Temp > 10:
+    print('its mild')
+else:
+    print('its cold')
+print('Thank you')
