2adeaf31cacb2aefdb13d3284acece3e9da84746
Logan Zimmerman <lz128152@student.wpsok.org>
2024-10-29 13:32:13 -0500

    added HW10

---
+++ HW10.py
@@ -0,0 +1,26 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW10
+
+#1. Create a while loop with variable i that counts down from 5 to 0
+#and then prints "Hello World!" at the end.
+i = 5
+while i >=0:
+    print(i)
+    i -=1
+#2. Create a while loop that prints only even numbers
+#between 1 and 30.
+print()
+j = 2
+while j <=30:
+    print(j)
+    j+=2
+#3. Create a while loop that repeats until the user
+#inputs the number 0.
+f=1
+while f==1:
+    print(f)
+    r=input()
+    if r == '0':
+        exit()
+
