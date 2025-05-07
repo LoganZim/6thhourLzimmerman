67b77f63624660aee9f93f497050927c25ae3e95
logan <lz128152@student.wpsok.org>
2025-05-07 18:22:37 -0500

    added playground

--- playground2.py
+++ playground2.py
@@ -1,46 +1,32 @@
-<<<<<<< HEAD
+
 import random
 import time
-=======
 
-def w(msg):
-    while True:
-        try:
->>>>>>> origin/master
 
 balance = 1000
 pot = 0
-faces = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-suits = ["\u2664", "\u2661", "\u2662", "\u2667"]
-deck = []
-table = []
-hand = []
-
-for i in range(0, len(faces)):
-    for j in range(0, len(suits)):
-        card = (faces[i], suits[j])
-        deck.append(card)
-random.shuffle(deck)
 
 print('This poker game isnt multiplayer but the better hand you get gets you more \n '
       'money and your goal is to make the most money you can and whatever you bet you get more back depending on the strength of your hand\n'
       'but if your hand isnt good you lose money\n')
 
-
-<<<<<<< HEAD
 while True:
+    faces = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    suits = ["\u2664", "\u2661", "\u2662", "\u2667"]
+    deck = []
+    table = []
+    hand = []
+    break1 = 0
+    for i in range(0, len(faces)):
+        for j in range(0, len(suits)):
+            card = (faces[i], suits[j])
+            deck.append(card)
+    random.shuffle(deck)
+
     if balance <= 0:
-        break
-=======
-print('Enter 1 for black jack\n')
-print('Enter 2 for poker')
-
-choice = w('enter choice: ')
-
-while True:
-    if choice==1:
-
->>>>>>> origin/master
+        time.sleep(1)
+        print('\nyou ran out of money')
+        exit()
 
     def draw():
         global hand
@@ -108,25 +94,6 @@ while True:
         time.sleep(3)
 
 
-    def bet_2():
-        global pot
-        global balance
-
-        print('Your balance is', balance)
-        bet2 = int(input('How much do you want to bet: '))
-        if bet2 > balance:
-            print('Cant bet more than what you have')
-            bet_1()
-        elif bet2 < 0:
-            print('Cant bet less that 0')
-            bet_1()
-        else:
-            pot += bet2
-            balance -= bet2
-            print(' The pot is now', pot)
-            print()
-
-
     def turn():
         global hand
         global table
@@ -171,28 +138,31 @@ while True:
 
 
     def pair():
-        global hand, table, pot, balance
+        global hand, table, pot, balance, break1
 
-        for i in range(5):
-            if hand[0][0] == table[i][0]:
+        for l in range(5):
+            if hand[0][0] == table[l][0]:
                 print(f"You get 50% back with a pair of {hand[0][0]}'s")
                 balance += pot/2
                 break
-            elif hand[1][0] == table[i][0]:
+            elif hand[1][0] == table[l][0]:
                 print(f"You get 50% back with a pair of {hand[1][0]}'s")
                 balance += pot/2
                 break
-
-            if hand[0][0] == hand[1][0]:
+            elif hand[0][0] == hand[1][0]:
                 print(f"You get 50% back with a pair of {hand[0][0]}'s")
                 balance += pot/2
                 break
             for k in range(5):
                 for o in range(k + 1, 5):
                     if table[k][0] == table[o][0]:
-                        print(f"There is a pair of {table[k][0]}'s")
+                        print(f"You get 50% back with a pair of {table[k][0]}'s")
+                        break1 += 1
                         break
-
+            if break1 == 1:
+                break
+        time.sleep(2)
+        print('Next round')
 
 
 
@@ -205,7 +175,6 @@ while True:
     flop()
     bet_1()
     turn()
-    bet_2()
     river()
 
     three_of_a_kind()
